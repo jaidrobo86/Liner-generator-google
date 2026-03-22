bf26a38cc3be01e264fb4e4a6a9d2f177482f636
jaidrobo86 <jaidrobo@gmail.com>
2026-03-22 19:22:59 +1000

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,10 @@ def generate():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+if __name__ == "__main__":
+    # Google Cloud tells your app which port to use via an environment variable
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",
